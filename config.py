51eae320565528bfa9c1a400282c376c914534a4
francisco.veles <francisco.veles@gmail.com>
2023-04-25 20:43:03 -0700

    Actualizacion de configuracion para GIT

--- config.py
+++ config.py
@@ -2,7 +2,8 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-#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
+#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}
+#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
 class Config:
     # SECRET_KEY = os.environ.get('SECRET_KEY')
     SECRET_KEY = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
